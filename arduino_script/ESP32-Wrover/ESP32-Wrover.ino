f4a22abbca8ea5ff662e510e2a4f4d8d1a2efd31
Widad FjrY <widadzdadz@gmail.com>
2025-05-13 15:14:01 +0700

    fix bug

--- arduino_script/ESP32-Wrover/ESP32-Wrover.ino
+++ arduino_script/ESP32-Wrover/ESP32-Wrover.ino
@@ -3,60 +3,61 @@
 #include <HTTPClient.h>
 #include <PubSubClient.h>
 
-// #define FLASH_PIN 4
-
 // PIN GPIO yang digunakan
-#define PIR_PIN_1 13    
-#define PIR_PIN_2 12     
+#define PIR_PIN_1 13
+#define PIR_PIN_2 15
 #define PIR_PIN_3 14
 
-#define BUZZER_PIN 33   
+#define BUZZER_PIN 33
 #define LED_GREEN 32
 
 // PIN Kamera
-#define PWDN_GPIO_NUM  -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM  21
-#define SIOD_GPIO_NUM  26
-#define SIOC_GPIO_NUM  27
 
-#define Y9_GPIO_NUM    35
-#define Y8_GPIO_NUM    34
-#define Y7_GPIO_NUM    39
-#define Y6_GPIO_NUM    36
-#define Y5_GPIO_NUM    19
-#define Y4_GPIO_NUM    18
-#define Y3_GPIO_NUM    5
-#define Y2_GPIO_NUM    4
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 21
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 19
+#define Y4_GPIO_NUM 18
+#define Y3_GPIO_NUM 5
+#define Y2_GPIO_NUM 4
 #define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM  23
-#define PCLK_GPIO_NUM  22
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 // ID Sensor
-const char* pirId1 = "SENPIR0001";
-const char* pirId2 = "SENPIR0002";
-const char* pirId3 = "SENPIR0003";
+const char *pirId1 = "SENPIR0001";
+const char *pirId2 = "SENPIR0002";
+const char *pirId3 = "SENPIR0003";
 
 bool isPir1Active = true;
 bool isPir2Active = true;
 bool isPir3Active = true;
 
 // Konfigurasi WIFI & Endpoint server
-const char* ssid = "RYUJIN";
-const char* password = "RyuJin1029";
-const char* serverUrl = "https://farm.dihara.my.id/api/";
+const char *ssid = "POCO F5";
+const char *password = "TofuGoreng";
+// const char* serverUrl = "http://192.168.1.7:8080/api/";
+const char *serverUrl = "http://farm.dihara.my.id/api/";
 
 // Konfigurasi Broker
-const char* mqttServer = "broker.hivemq.com";
+const char *mqttServer = "broker.hivemq.com";
 const int mqttPort = 1883;
-const char* mqttTopic = "bido_dihara/broker/farm-security";
+const char *mqttTopic = "bido_dihara/broker/farm-security";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 bool takePhoto = false;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   startCamera();
   connectToWiFi();
@@ -68,13 +69,18 @@ void setup() {
   pinMode(PIR_PIN_3, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
-
-  digitalWrite(LED_GREEN, HIGH);
 }
 
-void loop() {
-  if (!client.connected()) {
-      reconnectMQTT();
+void loop()
+{
+  if (!client.connected())
+  {
+    digitalWrite(LED_GREEN, LOW);
+    reconnectMQTT();
+  }
+  else
+  {
+    digitalWrite(LED_GREEN, HIGH);
   }
   client.loop();
 
@@ -82,25 +88,29 @@ void loop() {
   bool motionDetected2 = isMotionDetected(PIR_PIN_2);
   bool motionDetected3 = isMotionDetected(PIR_PIN_3);
 
-  if (motionDetected1 && isPir1Active) {
-      Serial.println("Gerakan terdeteksi oleh PIR 1");
-      buzzerActive();
-      sendPhoto(pirId1, false);
+  if (motionDetected1 && isPir1Active)
+  {
+    Serial.println("Gerakan terdeteksi oleh PIR 1");
+    buzzerActive();
+    sendPhoto(pirId1, false);
   }
-  if (motionDetected2 && isPir2Active) {
-      Serial.println("Gerakan terdeteksi oleh PIR 2");
-      buzzerActive();
-      sendPhoto(pirId2, false);
+  if (motionDetected2 && isPir2Active)
+  {
+    Serial.println("Gerakan terdeteksi oleh PIR 2");
+    buzzerActive();
+    sendPhoto(pirId2, false);
   }
-  if (motionDetected3 && isPir3Active) {
-      Serial.println("Gerakan terdeteksi oleh PIR 3");
-      buzzerActive();
-      sendPhoto(pirId3, false);
+  if (motionDetected3 && isPir3Active)
+  {
+    Serial.println("Gerakan terdeteksi oleh PIR 3");
+    buzzerActive();
+    sendNotification(pirId3);
   }
-  delay(300);
+  delay(1000);
 }
 
-void startCamera() {
+void startCamera()
+{
   camera_config_t config;
   config.pin_d0 = Y2_GPIO_NUM;
   config.pin_d1 = Y3_GPIO_NUM;
@@ -122,117 +132,156 @@ void startCamera() {
   config.pixel_format = PIXFORMAT_JPEG;
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.frame_size = FRAMESIZE_SVGA;  
-  config.jpeg_quality = 20;
+
+  config.frame_size = FRAMESIZE_VGA;
+  config.jpeg_quality = 30;
   config.fb_count = 1;
-  
+
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-      Serial.printf("Kamera Gagal Inisialisasi Dengan Error 0x%x", err);
-      return;
+  if (err != ESP_OK)
+  {
+    Serial.printf("Kamera Gagal Inisialisasi Dengan Error 0x%x", err);
+    return;
   }
 }
 
-void connectToWiFi() {
+void connectToWiFi()
+{
   WiFi.begin(ssid, password);
   Serial.print("Menghubungkan ke WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("\nTersambung ke WiFi");
 }
 
-void reconnectMQTT() {
+void reconnectMQTT()
+{
   Serial.print("Menyambung ke MQTT...");
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     String clientId = "ESP32Cam-" + String(random(0xffff), HEX);
-    if (client.connect(clientId.c_str())) {
+    if (client.connect(clientId.c_str()))
+    {
       Serial.println("Tersambung ke MQTT");
       client.publish(mqttTopic, "Device online");
       client.subscribe(mqttTopic);
-    } else {
+    }
+    else
+    {
       Serial.print("Gagal. rc=");
       Serial.print(client.state());
-      Serial.println(" coba lagi dalam 2 detik.");
-      delay(2000);
     }
   }
 }
 
-void sendPhoto(const char* deviceId, bool isFromUser) {
-  delay(200); 
-  camera_fb_t* fb2 = esp_camera_fb_get();
-  if (!fb2) {
-      Serial.println("Gagal Mengambil Gambar");
-      return;
-  }
-  delay(300);
+void sendPhoto(const char *deviceId, bool isFromUser)
+{
+  camera_fb_t *fb2 = esp_camera_fb_get();
   esp_camera_fb_return(fb2);
-  
-  camera_fb_t* fb = esp_camera_fb_get();
-  if (!fb) {
-      Serial.println("Gagal Mengambil Gambar");
-      return;
+  Serial.println("Sukses initial gambar");
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (!fb)
+  {
+    Serial.println("Gagal Mengambil Gambar");
+    return;
+  }
+  else
+  {
+    Serial.println("Sukses Mengambil Gambar");
   }
 
-  delay(100);
   randomSeed(millis());
-
   String pictureId = generateRandomString(15);
-  if (WiFi.status() == WL_CONNECTED) {
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
     HTTPClient http;
+    http.useHTTP10(true);
+    http.setTimeout(10000);
+
     String fullUrlUpload = String(serverUrl) + "upload/" + pictureId;
     http.begin(fullUrlUpload);
     http.addHeader("Content-Type", "image/jpeg");
+    http.addHeader("Connection", "close");
+
     int httpResponseCode = http.POST(fb->buf, fb->len);
-    if (httpResponseCode > 0) {
-      String payload = "ok pictureId: " + pictureId;
-      Serial.printf("Gambar Terkirim! Kode Respon: %d\n", httpResponseCode);
-      client.publish(mqttTopic, payload.c_str());
-    } else {
-      Serial.printf("Gagal Mengirim Gambar. Error: %s\n", http.errorToString(httpResponseCode).c_str());
-    }
+    Serial.println(httpResponseCode);
+    Serial.println("Sukses Menggirim Gambar" + pictureId);
+
+    esp_camera_fb_return(fb);
     http.end();
-    delay(300);
-    if (!isFromUser){
+
+    if (!isFromUser)
+    {
       HTTPClient http2;
       String fullUrlMotion = String(serverUrl) + "motion-detected/" + pictureId;
-      http2.setTimeout(10000);
       http2.begin(fullUrlMotion);
       http2.addHeader("Content-Type", "application/json");
+      http2.addHeader("Connection", "close");
       String jsonBody = "{\"device_id\": \"" + String(deviceId) + "\", \"motion_detected\": true}";
-      http2.POST(jsonBody);
+      int httpResponseCode2 = http2.POST(jsonBody);
+      Serial.println(httpResponseCode2);
       http2.end();
     }
-  } else {
+  }
+  else
+  {
     Serial.println("WiFi Tidak Terhubung.");
   }
-  esp_camera_fb_return(fb);
 }
 
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
+void sendNotification(const char *deviceId)
+{
+  randomSeed(millis());
+  String motionId = generateRandomString(15);
+
+  HTTPClient http;
+  String fullUrlMotion = String(serverUrl) + "motion-detected/" + motionId;
+  http.begin(fullUrlMotion);
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("Connection", "close");
+  String jsonBody = "{\"device_id\": \"" + String(deviceId) + "\", \"motion_detected\": true}";
+  int httpResponseCode2 = http.POST(jsonBody);
+  Serial.println(httpResponseCode2);
+  http.end();
+}
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Pesan Diterima [");
   Serial.print(topic);
   Serial.print("]: ");
   String message;
-  for (int i = 0; i < length; i++) {
-      message += (char)payload[i];
+  for (int i = 0; i < length; i++)
+  {
+    message += (char)payload[i];
   }
   Serial.println(message);
 
-  if (message == "TAKE_PHOTO") {
-    sendPhoto(pirId1, true); 
-  } else if (message == "BUZZER_ON") {
+  if (message == "TAKE_PHOTO")
+  {
+    sendPhoto(pirId1, true);
+  }
+  else if (message == "BUZZER_ON")
+  {
     buzzerActive();
     client.publish(mqttTopic, "ok");
-  } else if (message == "ALARM_ON") {
+  }
+  else if (message == "ALARM_ON")
+  {
     alarmActive();
     client.publish(mqttTopic, "ok");
-  } else if (message == "ALARM_OFF") {
+  }
+  else if (message == "ALARM_OFF")
+  {
     alarmNonActive();
     client.publish(mqttTopic, "ok");
-  } else if (message.startsWith("DISABLE SENSOR")){
+  }
+  else if (message.startsWith("DISABLE SENSOR"))
+  {
     int idStart = message.indexOf("SENSOR ID: ") + strlen("SENSOR ID: ");
     int idEnd = message.indexOf(",", idStart);
     String sensorId = message.substring(idStart, idEnd);
@@ -241,43 +290,53 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
 
     bool isActive = isActiveStr == "true";
 
-    if (sensorId == pirId1){
+    if (sensorId == pirId1)
+    {
       isPir1Active = isActive;
-    } else if (sensorId == pirId2){
+    }
+    else if (sensorId == pirId2)
+    {
       isPir2Active = isActive;
-    } else if (sensorId == pirId3){
+    }
+    else if (sensorId == pirId3)
+    {
       isPir3Active = isActive;
     }
     client.publish(mqttTopic, "ok");
   }
 }
 
-String generateRandomString(int length) {
-  const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+String generateRandomString(int length)
+{
+  const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*";
   String randomString;
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     randomString += charset[random(sizeof(charset) - 2)];
   }
   return randomString;
 }
 
-void buzzerActive(){
+void buzzerActive()
+{
   digitalWrite(BUZZER_PIN, HIGH);
   delay(500);
   digitalWrite(BUZZER_PIN, LOW);
 }
 
-void alarmActive(){
+void alarmActive()
+{
   digitalWrite(BUZZER_PIN, HIGH);
   client.publish(mqttTopic, "ok");
 }
 
-void alarmNonActive(){
+void alarmNonActive()
+{
   digitalWrite(BUZZER_PIN, LOW);
   client.publish(mqttTopic, "ok");
 }
 
-bool isMotionDetected(int pin) {
+bool isMotionDetected(int pin)
+{
   return digitalRead(pin) == HIGH;
 }
-
